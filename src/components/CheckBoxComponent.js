eb71983d1f794f64c5ec9bc06452b90aea0f8c20
Charan Romi <charanromi325@gmail.com>
2024-10-23 16:06:35 +0530

    new components

---
+++ src/components/CheckBoxComponent.js
@@ -0,0 +1,12 @@
+const CheckBoxComponent = () => {
+    return (
+        <div>
+          <h4>React Checkbox Component</h4>
+          <label>
+            <input type="checkbox" /> Check me!
+          </label>
+        </div>
+      );
+};
+
+export default CheckBoxComponent;
---
+++ src/components/TextComponent.js
@@ -0,0 +1,9 @@
+const TextComponent = () => {
+    return (
+        <div>
+          <h4>Editable Component</h4>
+        </div>
+      );
+}
+
+export default TextComponent;
